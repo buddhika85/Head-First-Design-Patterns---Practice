643c991c2e75ad2da7f8375f22c0229570ae24df
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2026-03-11 03:32:57 +1100

    strategy pattern - delegates version

---
+++ HeadFirstDesignPatterns/1_Strategy_Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Payer payer = new() { PaymentStrategy = PaymentStrategies.CreditCardStrategy };
+payer.MakePayment(100);
+
+// flexible change of payment strategy
+payer.PaymentStrategy = PaymentStrategies.PayPalStrategy;
+payer.MakePayment(101);
+
+
+public static class PaymentStrategies
+{
+    public static Action<decimal> CreditCardStrategy = (amount) => Console.WriteLine($"Credit card payment of {amount}");
+    public static Action<decimal> PayPalStrategy = (amount) => Console.WriteLine($"Paypal payment of {amount}");
+}
+
+
+public class Payer
+{
+    public required Action<decimal> PaymentStrategy { get; set; }              // HAS-A Relationship - COMPOSITION
+
+    public void MakePayment(decimal amount)
+    {
+        PaymentStrategy(amount);
+    }
+}
---
+++ HeadFirstDesignPatterns/RecordsInC#/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Test code - records
+var personR1 = new RecordsDemo.Person("John", "Smith");
+var personR2 = new RecordsDemo.Person("Jane", "Smith");
+var personR3 = new RecordsDemo.Person("John", "Smith");
+Console.WriteLine(personR1);                        // records toString() implemenation is auto generated
+Console.WriteLine(personR2);
+Console.WriteLine(personR3);
+Console.WriteLine(personR1 == personR2);
+Console.WriteLine(personR1 == personR3);            // value based equality - so true because attribute values are same
+
+//personR1.FirstName = "Jack";                      // records properties are by-default init only, so cannot reassign
+
+Console.WriteLine(personR1.Equals(personR2));
+Console.WriteLine(personR1.Equals(personR3));
+Console.WriteLine(personR1.GetHashCode());                        
+Console.WriteLine(personR2.GetHashCode());
+Console.WriteLine(personR3.GetHashCode());
+
+Console.WriteLine(personR1.GetHashCode() == personR3.GetHashCode());            // same hash code 2 record instances with same property values
+
+
+
+//// Test code - classes
+//var personC1 = new ClassDemo.Person { FirstName = "John", LastName = "Smith" };
+//var personC2 = new ClassDemo.Person { FirstName = "Jane", LastName = "Smith" };
+//var personC3 = new ClassDemo.Person { FirstName = "John", LastName = "Smith" };
+//Console.WriteLine(personC1);                        // classes toString() implemenation is Not auto generated
+//Console.WriteLine(personC2);
+//Console.WriteLine(personC3);
+
+//Console.WriteLine(personC1 == personC2);
+//Console.WriteLine(personC1 == personC3);            // reference based equality - so false even though the attribute values are same
+
+//personC1.FirstName = "Test";                        // classes attributes can be reassigned
+
+namespace RecordsDemo 
+{
+    //public record Person(string FirstName, string LastName);
+
+    // above is short hand notation for
+    public record Person
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
+
+
+namespace ClassDemo
+{
+    public class Person
+    { 
+        public required string FirstName { get; set; } 
+        public required string LastName { get; set; }
+    }
+}
