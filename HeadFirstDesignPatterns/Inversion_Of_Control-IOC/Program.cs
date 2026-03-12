8b3d7cf436782c3667af6364f028fa1ad603f08d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2026-03-13 05:06:18 +1100

    Inversion of control - call backs example

---
+++ HeadFirstDesignPatterns/Inversion_Of_Control-IOC/Program.cs
@@ -0,0 +1,11 @@
+﻿new IOC().Greet("Buddhika", () => { Console.WriteLine("Program Finished."); });
+
+class IOC
+{
+    public void Greet(string name, Action callback)
+    {
+        Console.WriteLine($"Hello: {name} !");
+
+        callback();
+    }
+}
